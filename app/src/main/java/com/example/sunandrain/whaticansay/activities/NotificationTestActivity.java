1dc6e6749c0332d9a349a8e0d88667b16346cd23
broccoliscalded <1728131835@qq.com>
2017-09-29 16:21:10 +0800

    What a big

--- app/src/main/java/com/example/sunandrain/whaticansay/activities/NotificationTestActivity.java
+++ app/src/main/java/com/example/sunandrain/whaticansay/activities/NotificationTestActivity.java
@@ -4,19 +4,37 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.sunandrain.whaticansay.R;
 
-public class NotificationTestActivity extends AppCompatActivity {
+public class NotificationTestActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static Intent newIntent(Context context){
-        Intent intent = new Intent(context,NotificationTestActivity.class);
-        return intent;
+        return new Intent(context,NotificationTestActivity.class);
+//        return intent;
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_test);
+
+        initView();
+    }
+
+    private void initView() {
+        findViewById(R.id.button).setOnClickListener(this);
+        findViewById(R.id.button2).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button:
+                break;
+            case R.id.button2:
+                break;
+        }
     }
 }
